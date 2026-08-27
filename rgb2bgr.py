440eee70edbad9fd02570e5d42dc7facd7c8177c
Aadit <aadits2004@gmail.com>
2026-08-27 15:38:56 +0530

    changed colors

--- rgb2bgr.py
+++ rgb2bgr.py
@@ -1,3 +1,18 @@
 # trying to convert an image from rgb to bgr and see what it looks like
 
-# def convert() -> None:
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread("images/input.jpg")
+
+# OpenCV reads it as BGR.
+# Convert BGR -> RGB for matplotlib.
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+# Deliberately swap RGB -> BGR and display it as if it were RGB.
+bgr_as_rgb = rgb[:, :, ::-1]
+
+plt.imshow(bgr_as_rgb)
+plt.axis("off")
+# plt.show()
+plt.savefig("images/result.png")
